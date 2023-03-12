de352a23d51653a7314493f5a1b0b9b5b5645400
Anderson Sales <andersonsales.mg@gmail.com>
2023-03-12 01:00:22 -0300

    Create Project base

---
+++ CodableDecodable/CodableDecodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableDecodableApp.swift
+//  CodableDecodable
+//
+//  Created by Anderson Sales on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CodableDecodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
